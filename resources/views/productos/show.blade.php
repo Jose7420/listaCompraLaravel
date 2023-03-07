9969d5a982485f40a775c12f2ae22cd7633c8203
Jose7420 <7962446@alu.murciaeduca.es>
2023-03-07 11:27:56 +0100

    seguir en casa

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,5 +2,19 @@
 
 @section('content')
 
-    <h1>Vista detalle producto {id}/h1>
+    <h1>Vista detalle producto {{$id}}</h1>
+        <div class="row">
+
+            <div class="col-sm-4">
+
+                {{-- TODO: Imagen genérica de los productos --}}
+                {{ $id ?? '' }}
+
+            </div>
+            <div class="col-sm-8">
+
+                {{-- TODO: Datos del producto --}}
+
+            </div>
+        </div>
 @stop
